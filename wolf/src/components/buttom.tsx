c2852fb9aacb5896140865d7334a41dcabcec0ef
mauro-ezequiel <129565868+mauro-ezequiel@users.noreply.github.com>
2024-04-12 12:58:04 -0300

    add links to user's networks

--- wolf/src/components/buttom.tsx
+++ wolf/src/components/buttom.tsx
@@ -10,7 +10,32 @@ export const Buttom: React.FC<butt> = ({}: butt) => {
           {" "}
           puedes contactarnos en las siguientes redes{" "}
         </div>
-        <div className="cont link">w</div>
+        <div className="cont link">
+          <a href="https://github.com/mauro-ezequiel" target="-blank">
+            <i className="bi bi-github"></i>
+          </a>
+          <a
+            href="https://www.facebook.com/profile.php?id=100088989336658"
+            target="-blank"
+          >
+            <i className="bi bi-facebook"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/black_wolf.3d/?hl=es"
+            target="-blank"
+          >
+            <i className="bi bi-instagram"></i>
+          </a>
+          <a href="https://wa.me/541150941389" target="-blank">
+            <i className="bi bi-whatsapp"></i>
+          </a>
+          <a
+            href="https://mail.google.com/mail/u/0/#search/from%3A%40jooble.org+in%3Aanywhere/FMfcgzGtwCtTwJMFtBCXBlHLrcdTZmpk"
+            target="-blank"
+          >
+            <i className="bi bi-envelope-at-fill"></i>
+          </a>
+        </div>
       </div>
     </div>
   );
